6596d741e1a2c72486583c303b26d4e927d8332e
Vincent <vincentbolinget@gmail.com>
2024-03-30 10:04:56 +0800

    work experience section added

--- public/index.js
+++ public/index.js
@@ -1,6 +1,9 @@
-
-const child = document.querySelector('#civil-container');
-
-console.log(
-    child.lastElementChild
-)
+const delButtons = document.querySelectorAll('.del-button');
+const civilContainer = document.querySelector('#civil-container');
+        console.log(civilContainer)
+delButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const entry = button.closest('.civil-entry');
+        civilContainer.removeChild(entry);
+    })
+})
